7e2f6fa3d438a7b6ec25170e6df10307d66e3770
Pedro J. Molina <pjmolina@gmail.com>
2016-09-27 17:48:12 +0200

    app2

--- App1/Properties/AssemblyInfo.cs
+++ App1/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("App1 hotfix 23")]
+[assembly: AssemblyTitle("App2 hotfix 23")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
