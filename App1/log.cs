ddf50c1723a488b76e7744684ddd57c0d9746753
Pedro J. Molina <pjmolina@gmail.com>
2016-09-27 17:02:56 +0200

    cambio 34

---
+++ App1/log.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola");
+			// OJO
+            Console.WriteLine("Adios");
+        }
+    }
+}
