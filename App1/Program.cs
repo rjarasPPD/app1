dc5a927237675b22f06e1c9be961f3ed69283fe0
Pedro J. Molina <pjmolina@gmail.com>
2016-09-27 17:39:28 +0200

    añadidas 5 lineas

--- App1/Program.cs
+++ App1/Program.cs
@@ -12,8 +12,12 @@ namespace App1
         {
             Console.WriteLine("Hola");
             //Añado una linea
-            Console.WriteLine("Entre lineas");
-	    // OJO
+            Console.WriteLine("Entre lineas 1");
+            Console.WriteLine("Entre lineas 2");
+            Console.WriteLine("Entre lineas 3");
+            Console.WriteLine("Entre lineas 4");
+            Console.WriteLine("Entre lineas 5");
+            // OJO
             Console.WriteLine("Adios");
         }
     }
